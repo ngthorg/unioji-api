938dea8fa92138bafce9bb9251323be1f37e4a87
ngthong <ngthorg@gmail.com>
2020-06-28 17:52:32 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/unioji/unioji-api
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.0.1
+)
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,37 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/unioji/unioji-api/graph/generated"
+	"github.com/unioji/unioji-api/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Viewer(ctx context.Context) (*model.Viewer, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Node(ctx context.Context, id string) (model.Node, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Search(ctx context.Context, text string) ([]model.SearchResult, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
